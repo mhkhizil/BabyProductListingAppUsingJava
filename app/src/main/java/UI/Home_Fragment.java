3a4a0cfb094c1322e944da310de91eb42fc661e0
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2024-07-22 07:39:10 +0630

    adding ui  modification

--- app/src/main/java/UI/Home_Fragment.java
+++ app/src/main/java/UI/Home_Fragment.java
@@ -1,5 +1,6 @@
 package UI;
 
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.assignment.R;
 
@@ -19,7 +21,26 @@ public class Home_Fragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View home_view = inflater.inflate(R.layout.fragment_home, container, false);
+        Button startButton = home_view.findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putString("mode", "add_mode");
+                bundle.putString("username", "your_username");
+
+                Property_Form_Fragment p_form_fragment = new Property_Form_Fragment();
+                p_form_fragment.setArguments(bundle);
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, p_form_fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return home_view;
     }
 
 }
